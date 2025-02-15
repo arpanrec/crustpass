12e1295f814a78f66c3c796d9c5bbe0abffb59a4
arpan <arpan.rec@gmail.com>
2025-02-16 04:12:29 +0530

    fix: Rename in progress

--- src/routers/mod.rs
+++ src/routers/mod.rs
@@ -1,13 +1,8 @@
 mod secret;
 
 use crate::AppState;
-use axum::{
-    routing::{get, post},
-    Router,
-};
+use axum::{routing::get, Router};
 
 pub(super) fn get_secret_router() -> Router<AppState> {
-    Router::new()
-        .route("/{*key}", get(secret::handle_get).delete(secret::handle_delete))
-        .route("/{*key}", post(secret::handle_post))
+    Router::new().route("/{*key}", get(secret::handle_get).post(secret::handle_post).delete(secret::handle_delete))
 }
