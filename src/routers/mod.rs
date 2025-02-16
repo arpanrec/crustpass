5f44cb80d6ad7eefd5d82e8ed9a88f51a189b7c7
arpan <arpan.rec@gmail.com>
2025-02-16 17:55:26 +0530

    chor: make get_secret_router public

--- src/routers/mod.rs
+++ src/routers/mod.rs
@@ -3,6 +3,6 @@ mod secret;
 use crate::AppState;
 use axum::{routing::get, Router};
 
-pub(super) fn get_secret_router() -> Router<AppState> {
+pub fn get_secret_router() -> Router<AppState> {
     Router::new().route("/{*key}", get(secret::handle_get).post(secret::handle_post).delete(secret::handle_delete))
 }
